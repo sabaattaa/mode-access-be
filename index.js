21d62bd95312b2f583c05c009dcef872ca661146
Ahmad1812 <ahmadimran1812@gmail.com>
2026-02-10 19:44:29 +0500

    updated mongo db

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ import { fileURLToPath } from "url";
 
 // connectDB();
 const app = express();
-app.use(cors({ origin: "*" })); 
+app.use(cors( {
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+})); 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 

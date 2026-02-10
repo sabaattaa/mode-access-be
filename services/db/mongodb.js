9657b4b23476229adccffcecb1b029b6d7ac6334
Ahmad1812 <ahmadimran1812@gmail.com>
2026-02-10 19:00:02 +0500

    convert express to vercel serverless

--- services/db/mongodb.js
+++ services/db/mongodb.js
@@ -1,11 +1,28 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => { 
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI);
+//     console.log("MongoDB connected");
+//   } catch (error) {
+//     console.error("DB connection error:", error.message);
+//     process.exit(1);
+//   }
+// };
+
+
 import mongoose from "mongoose";
 
-export const connectDB = async () => { 
+let isConnected = false;
+
+export const connectDB = async () => {
+  if (isConnected) return;
+
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    const db = await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = db.connections[0].readyState;
     console.log("MongoDB connected");
   } catch (error) {
     console.error("DB connection error:", error.message);
-    process.exit(1);
   }
 };

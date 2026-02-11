fd370cb9e29e1a64b1dbcf31b2fa65eb581df3bf
Ahmad1812 <ahmadimran1812@gmail.com>
2026-02-11 12:56:45 +0500

    db connection update

--- api/index.js
+++ api/index.js
@@ -3,9 +3,6 @@ import { connectDB } from "../services/db/mongodb.js";
 
 export default async function handler(req, res) {
   try {
-    // 1. Pehle OPTIONS request (Preflight) handle karein
-    // Browser sirf yeh check karna chahta hai ke API allow hai ya nahi.
-    // Iske liye Database connect karne ki zaroorat nahi hai.
     if (req.method === "OPTIONS") {
       res.setHeader("Access-Control-Allow-Origin", "*");
       res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
@@ -13,10 +10,8 @@ export default async function handler(req, res) {
       return res.status(200).end();
     }
 
-    // 2. Sirf tab DB connect karein jab request OPTIONS nahi hai (e.g., POST, GET)
     await connectDB();
 
-    // 3. Request ko Express app par pass karein
     await app(req, res); 
 
   } catch (err) {
